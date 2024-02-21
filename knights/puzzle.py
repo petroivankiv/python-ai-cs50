d4756d0b861b07649d1d760046f391428f2a9b9b
Petro Ivankiv <petro.ivankiv88@gmail.com>
2024-02-21 23:03:26 +0200

    feat: solved puzzle

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -9,28 +9,50 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
+# Expected: A is a Knave
 knowledge0 = And(
     # Правила гри
-    Or(AKnight, AKnave),
-    
-    # Стартова версія
-    Implication(AKnight, AKnave),
+    Biconditional(AKnight, Not(AKnave)),
+
+    # A says "I am both a knight and a knave."
+    # Implication(AKnight, And(AKnight, AKnave)),
+    Or(Not(AKnight), And(AKnight, AKnave)),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+# Expected: A is a Knave, B is a Knight
 knowledge1 = And(
-    # TODO
+    # Правила гри
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+
+    # A says "We are both knaves."
+    Biconditional(AKnight, And(AKnave, BKnave)),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+# Expected: A is a Knave, B is a Knight
 knowledge2 = And(
-    # TODO
+    # Правила гри
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+
+    # A says "We are the same kind."
+    # Or(Not(AKnight), BKnight),
+    # Or(Not(AKnave), Not(BKnave)),
+    Biconditional(AKnight, Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+
+    # B says "We are of different kinds."
+    # Or(Not(BKnight), Not(AKnight)),
+    # Or(Not(BKnave), AKnave),
+    Biconditional(BKnight, Or(And(AKnave, BKnight), And(AKnight, BKnave))),
 )
 
 # Puzzle 3
@@ -38,8 +60,24 @@ knowledge2 = And(
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+# Expected: A is a Knight, B is a Knave, C is a Knight
 knowledge3 = And(
-    # TODO
+    # Правила гри
+    Biconditional(AKnight, Not(AKnave)),
+    Biconditional(BKnight, Not(BKnave)),
+    Biconditional(CKnight, Not(CKnave)),
+    
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Biconditional(AKnight, Or(AKnave, AKnight)),
+    
+    # B says "A said 'I am a knave'."
+    Implication(BKnight, Implication(AKnight, AKnave)),
+    
+    # B says "C is a knave."
+    Implication(BKnight, CKnave),
+    
+    # C says "A is a knight."
+    Biconditional(CKnight, AKnight),
 )
 
 
