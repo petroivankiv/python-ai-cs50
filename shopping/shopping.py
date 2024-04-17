32a60389155e0674f30aafd3e9b942e4060d7cd2
Petro Ivankiv <petro.ivankiv88@gmail.com>
2024-04-17 13:06:27 +0300

    feat: completed shopping task

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -6,15 +6,18 @@ from sklearn.neighbors import KNeighborsClassifier
 
 TEST_SIZE = 0.4
 
+MONTHS = dict(Jan=1, Feb=2, Mar=3, Apr=4, May=5, June=6, Jul=7, Aug=8, Sep=9, Oct=10, Nov=11, Dec=12)
+
 
 def main():
 
     # Check command-line arguments
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python shopping.py data")
+    # if len(sys.argv) != 2:
+    #     sys.exit("Usage: python shopping.py data")
 
     # Load data from spreadsheet and split into train and test sets
-    evidence, labels = load_data(sys.argv[1])
+    evidence, labels = load_data('shopping/shopping.csv')
+    
     X_train, X_test, y_train, y_test = train_test_split(
         evidence, labels, test_size=TEST_SIZE
     )
@@ -59,15 +62,45 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    evidence = []
+    labels = []
+    
+    with open(filename) as f:
+        reader = csv.DictReader(f)
 
+        for row in reader:
+           labels.append(1 if row['Revenue'] == 'TRUE' else 0)
+           evidence.append([
+               int(row['Administrative']),
+               float(row['Administrative_Duration']),
+               int(row['Informational']),
+               float(row['Informational_Duration']),
+               int(row['ProductRelated']),
+               float(row['ProductRelated_Duration']),
+               float(row['BounceRates']),
+               float(row['ExitRates']),
+               float(row['PageValues']),
+               float(row['SpecialDay']),
+               MONTHS[row['Month']],
+               int(row['OperatingSystems']),
+               int(row['Browser']),
+               int(row['Region']),
+               int(row['TrafficType']),
+               1 if row['VisitorType'] == 'Returning_Visitor' else 0,
+               1 if row['Weekend'] == 'TRUE' else 0,
+           ])
+           
+    
+    return (evidence, labels)
+    
 
 def train_model(evidence, labels):
     """
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    return neigh.fit(evidence, labels)
 
 
 def evaluate(labels, predictions):
@@ -85,7 +118,31 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    all_positive = 0
+    all_negative = 0
+    true_positive = 0
+    true_negative = 0
+    
+    for i, label in enumerate(labels):
+        if label == 1:
+            all_positive += 1
+            
+            if label == predictions[i]:
+                true_positive += 1
+                
+        if label == 0:
+            all_negative += 1
+            
+            if label == predictions[i]:
+                true_negative += 1
+                
+    if all_positive == 0:
+        all_positive = 1
+        
+    if all_negative == 0:
+        all_negative = 1
+            
+    return (true_positive / all_positive, true_negative / all_negative)
 
 
 if __name__ == "__main__":
